2f35c228fc5fd97263156a4546039f07687b19c6
msergeev <msergeev06@gmail.com>
2015-07-06 12:45:10 +0300

    Добавил локализацию файлу настроек

--- investtocar/public/setup.php
+++ investtocar/public/setup.php
@@ -1,6 +1,6 @@
 <? require_once($_SERVER["DOCUMENT_ROOT"]."/msergeev/investtocar/public/include/header.php"); ?>
-	<h1>Настройки</h1>
-<a href="<?=$path?>setup/add_car_model.php">Добавить модель автомобиля</a>
+	<h1><?=GetMessage("SETUP")?></h1>
+<a href="<?=$path?>setup/add_car_model.php"><?=GetMessage("ADD_AUTO_MODEL")?></a>
 
 
 <? require_once($_SERVER["DOCUMENT_ROOT"]."/msergeev/investtocar/public/include/footer.php"); ?>
