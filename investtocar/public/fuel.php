41caf874381e45e7c9be56931c1145a021ab46bb
msergeev <msergeev06@gmail.com>
2015-07-06 16:35:58 +0300

    Добавил новый раздел для обработки расходов на топливо

--- investtocar/public/fuel.php
+++ investtocar/public/fuel.php
@@ -1,5 +1,10 @@
 <? require_once($_SERVER["DOCUMENT_ROOT"]."/msergeev/investtocar/public/include/header.php"); ?>
 	<h1><?=GetMessage("FUEL")?></h1>
+<?
+	$defaultCar = CInvestToCarMain::GetDefaultCar();
+?>
+	<p><?=GetMessage("STATISTICS_FOR")?>: <? echo CInvestToCarMain::ShowSelectAuto("my_car",true); ?><br><br></p>
+	<p><a href="<?=$path?>fuel/add_fuel.php?car=<?=$defaultCar?>"><?=GetMessage("ADD_NOTE")?></a><br><br></p>
 
 
 
