fd2b0b7236ef4580ea8693f6c4637b2b359a6c70
msergeev <msergeev06@gmail.com>
2015-07-06 13:00:22 +0300

    Добавил локализацию в файл ts_delete.php

--- investtocar/public/ts/ts_delete.php
+++ investtocar/public/ts/ts_delete.php
@@ -1,4 +1,4 @@
 <? require_once($_SERVER["DOCUMENT_ROOT"]."/msergeev/investtocar/public/include/header.php"); ?>
-	<h1>Удаление записи о ТО</h1>
+	<h1><?=GetMessage("DELETE_NOTE_TS")?></h1>
 
 <? require_once($_SERVER["DOCUMENT_ROOT"]."/msergeev/investtocar/public/include/footer.php"); ?>
