7597758c2d5f64ecbb64a18f37bc95010a901731
msergeev <msergeev06@gmail.com>
2015-07-06 13:01:30 +0300

    Добавил локализацию в файл ts_edit.php

--- investtocar/public/ts/ts_edit.php
+++ investtocar/public/ts/ts_edit.php
@@ -1,4 +1,4 @@
 <? require_once($_SERVER["DOCUMENT_ROOT"]."/msergeev/investtocar/public/include/header.php"); ?>
-	<h1>Редактирование записи о ТО</h1>
+	<h1><?=GetMessage("EDIT_NOTE_TS")?></h1>
 
 <? require_once($_SERVER["DOCUMENT_ROOT"]."/msergeev/investtocar/public/include/footer.php"); ?>
